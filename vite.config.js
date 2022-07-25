ed42bbb292c815ef27065ed724742cd030a730dc
BB Caspian <casfian@gmail.com>
2022-07-25 22:08:29 +0800

    Changed Welcome page

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path'
 
 export default defineConfig({
     plugins: [
         laravel([
-            'resources/css/app.css',
             'resources/js/app.js',
         ]),
     ],
+    // This unless you can't find your bootstrap 5
+    //---
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
+    //---
 });
