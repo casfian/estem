125a3334933fff86fd43f29962fe7d0cbd7c8d9e
BB Caspian <casfian@gmail.com>
2022-07-25 22:11:06 +0800

    Change Welcome Page

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,8 +1,11 @@
-window._ = require('lodash');
+// window._ = require('lodash');
 
-try {
-    require('bootstrap');
-} catch (e) {}
+// try {
+//     require('bootstrap');
+// } catch (e) {}
+
+import loadash from 'lodash'
+window._ = loadash
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -10,9 +13,15 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+ import axios from 'axios'
+ window.axios = axios
+ 
+ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+ 
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// window.axios = require('axios');
+
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
